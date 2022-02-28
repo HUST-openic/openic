c36c8abbabbf7e81471c96d02aa8a69e3faec1c9
hzb <2324307175@qq.com>
2022-02-28 19:13:22 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
-  <v-app>
+  <v-app id="openic">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      v-app-bar
+    </v-app-bar>
+
+    <v-bottom-navigation app elevation="3">
+      bottom navigation
+    </v-bottom-navigation>
+
+    <v-navigation-drawer app>
+      v-navigation-drawer
+    </v-navigation-drawer>
+
+    <v-navigation-drawer app position="right">
+      navigation right
+    </v-navigation-drawer>
+
     <v-main>
       <HelloWorld/>
     </v-main>
+
   </v-app>
 </template>
 
