f0b4db76f6730d8a6089fcab9326f782631cdf76
hzb <2324307175@qq.com>
2022-03-01 15:21:07 +0800

    add xterm fit addon

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -12,12 +12,17 @@ import { onMounted } from "@vue/runtime-core";
 
 // Xtermjs basic import.
 import { Terminal } from "xterm";
-import "xterm/css/xterm.css"
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
 
-
+// Initiate terminal frontend once the dom is mounted.
 onMounted(() => {
-  var term = new Terminal();
+  const term = new Terminal();
+  const fitAddon = new FitAddon();
+
   term.open(document.getElementById('terminal'));
-  term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+  term.loadAddon(fitAddon);
+  fitAddon.fit();
+  term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 })
 </script>
