f5dd93f32271a1cc445e00957d41f35845f35176
hzb <2324307175@qq.com>
2022-03-04 23:35:33 +0800

    page layer

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,23 @@
 <template>
   <v-app id="openic">
-    <!-- <v-system-bar app top dark window elevation="5">
-      system bar
-    </v-system-bar> -->
-
     <v-app-bar app>
-        app bar
+      page select
     </v-app-bar>
 
-    <v-bottom-navigation app elevation="3">
-      bottom navigation
-    </v-bottom-navigation>
-
-    <v-navigation-drawer app>
-      v-navigation-drawer
-    </v-navigation-drawer>
-
-    <v-navigation-drawer app position="right">
-      navigation right
-    </v-navigation-drawer>
-
-    <v-main>
-      <HelloWorld/>
-      <Terminal/>
+    <v-main app>
+      <BasePage/>
     </v-main>
-
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Terminal from './components/Terminal.vue'
+import BasePage from './components/pages/BasePage.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
-    Terminal
+    BasePage
 },
 
   data: () => ({
---
+++ src/components/pages/BasePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+    <v-app-bar>
+      app bar
+    </v-app-bar>
+
+    <v-bottom-navigation elevation="3">
+      bottom navigation
+    </v-bottom-navigation>
+
+    <v-navigation-drawer>
+      v-navigation-drawer
+    </v-navigation-drawer>
+
+    <v-navigation-drawer position="right">
+      navigation right
+    </v-navigation-drawer>
+
+    <v-main>
+      <HelloWorld/>
+      <Terminal/>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+import HelloWorld from '../HelloWorld.vue';
+import Terminal from '../Terminal.vue';
+</script>
