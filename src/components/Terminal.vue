78d2277b77c96b614b857c7af9e21f5208404079
hzb <2324307175@qq.com>
2022-03-10 11:44:07 +0800

    update dependency

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -42,7 +42,7 @@ onMounted(() => {
   let cursorPos = 0; // Current position for the line buffer.
   
   // Write shell output for display in terminal.
-  term.write("touch /home/hzb/Desktop/itworks\r\necho hzb\u{1b}[?2004h\r\n\u{1b}[1;36mrustcmd\u{1b}[0m on \u{1b}[1;35m\u{e0a0} \u{1b}[0m\u{1b}[1;35mmain\u{1b}[0m \u{1b}[1;31m[\u{1b}[0m\u{1b}[1;31m!\u{1b}[0m\u{1b}[1;31m]\u{1b}[0m is \u{1b}[1;38;5;208m📦 \u{1b}[0m\u{1b}[1;38;5;208mv0.1.0\u{1b}[0m via \u{1b}[1;31m🦀 \u{1b}[0m\u{1b}[1;31mv1.59.0\u{1b}[0m\u{1b}[1;31m \u{1b}[0m\r\n\u{1b}[1;32m❯\u{1b}[0m touch /home/hzb/Desktop/itworks\r\n\u{1b}[?2004l\r\u{1b}[?2004h\r\n\u{1b}[1;36mrustcmd\u{1b}[0m on \u{1b}[1;35m\u{e0a0} \u{1b}[0m\u{1b}[1;35mmain\u{1b}[0m \u{1b}[1;31m[\u{1b}[0m\u{1b}[1;31m!\u{1b}[0m\u{1b}[1;31m]\u{1b}[0m is \u{1b}[1;38;5;208m📦 \u{1b}[0m\u{1b}[1;38;5;208mv0.1.0\u{1b}[0m via \u{1b}[1;31m🦀 \u{1b}[0m\u{1b}[1;31mv1.59.0\u{1b}[0m\u{1b}[1;31m \u{1b}[0m\r\n\u{1b}[1;32m❯\u{1b}[0m echo hzb\u{1b}[?2004l\r\u{1b}[?2004h\u{1b}[?2004l\r\r\n");
+  term.write("echo hzb\u{1b}[?2004h\r\n\u{1b}[1;36mrustcmd\u{1b}[0m on \u{1b}[1;35m\u{e0a0} \u{1b}[0m\u{1b}[1;35mfork\u{1b}[0m \u{1b}[1;31m[\u{1b}[0m\u{1b}[1;31m!\u{1b}[0m\u{1b}[1;31m]\u{1b}[0m is \u{1b}[1;38;5;208m📦 \u{1b}[0m\u{1b}[1;38;5;208mv0.1.0\u{1b}[0m via \u{1b}[1;31m🦀 \u{1b}[0m\u{1b}[1;31mv1.59.0\u{1b}[0m\u{1b}[1;31m \u{1b}[0m\r\n\u{1b}[1;32m❯\u{1b}[0m echo hzb\u{1b}[?2004l\r\u{1b}[?2004h\u{1b}[?2004l\r\r\n");
 
   term.onKey(e => {
     const ke = e.domEvent; // KeyboardEvent.
